170318a6af73290b24af11ef6a430256b1bb3f40
ddaannnnyy <hello@danhebdon.com>
2025-07-29 09:13:10 +1000

    fix: :zap: Redefined email check regex

--- utils/returnEmailValidity.ts
+++ utils/returnEmailValidity.ts
@@ -1,7 +1,7 @@
 export function returnEmailValidity(email: string) {
-    return !!String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/i
-      );
-  }
+  return !!String(email)
+    .toLowerCase()
+    .match(
+      /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$/i
+    );
+}
