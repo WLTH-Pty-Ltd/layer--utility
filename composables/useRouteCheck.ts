e5c8b7eb0f1ccb38bb321f96ab5963f7d600e7db
ddaannnnyy <hello@danhebdon.com>
2025-08-26 08:49:09 +1000

    fix: :bug: Removed console warn when resolving a path that does not exist

--- composables/useRouteCheck.ts
+++ composables/useRouteCheck.ts
@@ -1,9 +1,14 @@
 export function useRouteCheck(path: string) {
     const router = useRouter();
-    try {
-        const resolved = router.resolve(path);
-        return resolved.name !== undefined && resolved.name !== '404';
-    } catch {
-        return false;
+    const routeExists = router.hasRoute(path);
+    if (routeExists) {
+        try {
+            const resolved = router.resolve(path);
+            return resolved.name !== undefined && resolved.name !== '404';
+        } catch {
+            return false;
+        }
     }
+
+    return false;
 }
