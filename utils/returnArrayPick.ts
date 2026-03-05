1f0e71f9b4d3aa8d84743e04f01551bd412da070
ddaannnnyy <hello@danhebdon.com>
2026-03-05 10:18:41 +1000

    fixed typing for return array pick

--- utils/returnArrayPick.ts
+++ utils/returnArrayPick.ts
@@ -1,14 +1,25 @@
-export function returnArrayPick<T>(array: Array<Record<string | number, unknown>>, key: string | number | Array<string | number>, flatten?: boolean) {
+/***
+ * I'm using Array<any> here to get around a typescript limitation.
+ * Ideally I would use Array<string | number | Symbol> but this causes issues with Arrays of interfaces.
+ * I could just use Array<unknown> which is _technically_ correct, but means I have to cast everything inside of the for loops
+ * Which while _technically_ safer, causes a lot of Typescript jargon. This isn't an especially typescript heavy file and I want to keep the barrier
+ * of entry relatively low.
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function returnArrayPick<T>(array: Array<any>, key: string | number | Array<string | number>, flatten?: boolean) {
+  // This section is for nested arrays, which is the intended entry of this util. Flattening out arrays of arrays, but it will accept regular arrays
+  // Mostly to prevent typescript casting upstream
   if (Array.isArray(key)) {
     const newArray = [];
 
-    // check each entry of the array
+    // This cycles through the array looking for the specificed key. It then grabs the key and it's value and pushes them to the
+    // temporary newArray.
+    // I could probably replace this with a more efficent two point loop but it would cost readibility and it's typescript, calm down
     for (const item of array) {
       const entry: Partial<T> = {};
-      // check each key passes as a prop
       for (const k of key) {
         if (k in item) {
-          // if array key is in the prop assign it to the entry temporary value
           entry[k as keyof T] = item[k] as T[keyof T];
         }
       }
