4c12d92ad9d811d56882e1b5062f928305f67a0e
ddaannnnyy <hello@danhebdon.com>
2026-04-21 15:55:10 +1000

    :bug: fixed an issue where remove special character would crash if runtime undefined made it to the function. Now it returns the input if invalid

--- utils/returnRemoveSpecialCharacters.ts
+++ utils/returnRemoveSpecialCharacters.ts
@@ -1,4 +1,5 @@
 export function returnRemoveSpecialCharacters(input: string, replacementChar?: string) {
+    if (!input) return input;
     if (replacementChar) return input.replace(/[^a-zA-Z0-9]/g, replacementChar);
     else return input.replace(/[^a-zA-Z0-9]/g, '');
 }
